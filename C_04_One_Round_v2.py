9df28d7c04119050266fc162d14b76da9e315039
cherrypickles55 <susanths4014@masseyhigh.school.nz>
2026-02-27 12:14:57 +1300

    working single rounds

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -9,14 +9,27 @@ def initial_points(which_player):
     roll_one = random.randint (1, 6)
     roll_two = random.randint (1, 6)
 
-    if roll_one == roll_two:
-          double = "yes"
+    if roll_one + roll_two:
+        double = "yes"
 
 
     total = roll_one == roll_two
     print(f"{which_player} - Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total} ")
 
-     return total, double
+    return total, double
+
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
+
+
+
+
+
+
+# Main starts here...
+
 
 
 # Roll the dice for the user and note if they got a double
@@ -29,7 +42,6 @@ print("Initial Computer", initial_comp)
 # Retrieve user points (first item returned from function)
 user_points = initial_user [0]
 comp_points = initial_comp[0]
-
 
 double_user = initial_user[1]
 
@@ -67,8 +79,7 @@ while player_1_points < 13 and player_2_points < 13:
     player_1_roll = random.randint(1,6)
     player_1_points += player_1_roll
 
-     print(f'{second}: Rolled a {player_1_roll} - has {player_1_points} points")
-
+    print(f"{first}: Rolled a {player_1_roll} - has {player_1_points} points")
 
 
     # if the first person's score is over 13, end the round
@@ -80,8 +91,38 @@ while player_1_points < 13 and player_2_points < 13:
     player_2_points += player_2_roll
 
 
-    print(f'{second}: Rolled a {player_2_roll} - has {player_2_points} points")
+    print(f"{second}: Rolled a {player_2_roll} - has {player_2_points} points")
 
     print(f"{first}: {player_1_points} | {second} {player_2_points}")
 
-print("end of round")
+# end of round
+
+# associate player points with either the user or the computer
+user_points = player_1_points
+comp_points = player_2_points
+
+
+# switch the user computer points if the computer went first
+if first == "Computer":
+    user_points, comp_points = comp_points, user_points
+
+# work out who won...
+if user_points > comp_points:
+    winner = "user"
+else:
+    winner = "computer"
+
+round_feedback = f"The {winner} won."
+
+# double user points if eligible
+if winner == "user" and double_user == "yes":
+    user_points = user_points * 2
+
+
+# Output round results
+make_statement("Round Results", "=")
+print(f"User Points: {user_points} | Computer Points: {comp_points}")
+print(round_feedback)
+print()
+
+
